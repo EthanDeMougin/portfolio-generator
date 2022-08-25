b5a84f21b3d0e7b69750a353d0cd3c80760671dd
Ethan DeMougin <ethandemougin@gmail.com>
2022-08-25 15:50:19 -0400

    created an html :)

--- app.js
+++ app.js
@@ -1,15 +1,40 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
 const profileDataArgs = process.argv.slice(2);
 
-const printProfileData = profileDataArr => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
+console.log(profileDataArgs);
 
-  console.log('================');
+const [name, github] = profileDataArgs;
 
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+console.log(name, github);
 
-printProfileData(profileDataArgs);
+const pageHTML = generatePage(name, github);
+
+fs.writeFile('./index.html', pageHTML, err => {
+  if (err) throw err;
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
+
+
+
+//(1/3) ways to do this, all work!
+//(const generatePage = () => 'Name: Jane, Github: janehub'
+
+//console.log(generatePage());)
+
+//(2/3) ways to do this, all work!
+//(const generatePage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}`
+
+//console.log(generatePage('Jane', 'janehub'));)
+
+//(3/3) ways to do this, all work!
+//(const generatePage = (userName, githubName) => {
+  //return `
+    //Name: ${userName}
+    //Github: ${githubName}
+  //`;
+//};
+
+//console.log(generatePage('Jane', 'janehub'));)
